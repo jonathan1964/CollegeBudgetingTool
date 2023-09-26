b3d404019d32a2e3851d63c753cf5e6eda0613e6
jonathan1964 <95110344+jonathan1964@users.noreply.github.com>
2023-09-26 15:21:29 -0400

    Add files via upload
    Finalized proper input validation, character limits, fixed the customtkinter import again, and finalized all UI.

--- CollegeLoanTool.py
+++ CollegeLoanTool.py
@@ -120,18 +120,33 @@ window.slider_years.grid(row=2, column=0, padx=10, pady=(20,20), stick="w")
 #Defines Functions Related To Each Value Input, and Update the Left View Stats
 def update_pay():
     Pay = window.pay_entry.get()
-    window.pay_label.configure(text=Pay)
-    window.pay_label.after(25, update_pay)
-
+    
+    if len(Pay) > 0 and len(Pay) < 10 and Pay.isalpha() == False:
+        window.pay_label.configure(text=Pay)
+        window.pay_label.after(25, update_pay)
+    else:
+        window.pay_label.configure(text="Invalid")
+        window.pay_label.after(25, update_pay)
+    
 def update_hours():
     Hours = window.hours_entry.get()
-    window.hours_label.configure(text=Hours)
-    window.hours_label.after(25, update_hours)
+    
+    if len(Hours) > 0 and len(Hours) < 10 and Hours.isalpha() == False:
+        window.hours_label.configure(text=Hours)
+        window.hours_label.after(25, update_hours)
+    else:
+        window.hours_label.configure(text="Invalid")
+        window.hours_label.after(25, update_hours)
 
 def update_spending():
     Spending = window.spending_entry.get()
-    window.spending_label.configure(text=Spending)
-    window.spending_label.after(25, update_spending)
+
+    if len(Spending) > 0 and len(Spending) < 10 and Spending.isalpha() == False:
+        window.spending_label.configure(text=Spending)
+        window.spending_label.after(25, update_spending)
+    else:
+        window.spending_label.configure(text="Invalid")
+        window.spending_label.after(25, update_spending)
 
 def update_loan():
     Loan = window.slider_loan.get()
